a3b0a7dcde42e21142fda30fac2d3f5a9596ae4d
Melinda Winter <melindawinter42@gmail.com>
2020-09-18 15:00:23 -0700

    ready for heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,8 +27,7 @@ self.addEventListener("install", function (evt) {
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
+  // tell the browser to activate this service worker immediately once it has finished installing
   self.skipWaiting();
 });
 
