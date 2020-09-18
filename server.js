519054a2b9dff1dd3143222902b74855cd765419
Melinda Winter <melindawinter42@gmail.com>
2020-09-18 15:50:16 -0700

    fix server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0";
 
 const app = express();
 
@@ -28,6 +29,10 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
+
+app.listen(PORT, HOST, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
