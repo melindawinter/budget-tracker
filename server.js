efd1b81e6d14f6f4f863165efede9ea01e02957b
Melinda Winter <melindawinter42@gmail.com>
2020-09-21 10:17:46 -0700

    update server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+// Local host port for express
 const PORT = process.env.PORT || 3000;
 const HOST = "0.0.0.0";
 
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// Set up MONGODB connection
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/budget-tracker",
   {
@@ -26,12 +28,8 @@ mongoose.connect(
   }
 );
 
-// routes
+// Routes
 app.use(require("./routes/api.js"));
-
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
 
 app.listen(PORT, HOST, function () {
   console.log("Server listening on: http://localhost:" + PORT);
