1145adf7e629f8bdf7f63dd598800d1f64ebcfcd
Melinda Winter <melindawinter42@gmail.com>
2020-09-21 08:47:58 -0700

    review transaction file

--- models/transaction.js
+++ models/transaction.js
@@ -2,24 +2,25 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: "Enter a name for transaction"
-    },
-    value: {
-      type: Number,
-      required: "Enter an amount"
-    },
-    date: {
-      type: Date,
-      default: Date.now
-    }
+// Create schema for database
+const transactionSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction"
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount"
+  },
+  date: {
+    type: Date,
+    default: Date.now
   }
-);
+});
 
+// Set up the Transaction model so it can be exported
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
+// Export Transaction
 module.exports = Transaction;
